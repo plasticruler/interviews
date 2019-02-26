f2679e05057ac20df5564df8861779800d1bf7c9
marmite@dollar <mailbox@a20.co.za>
2019-02-26 07:10:40 +0200

    call to vendor service should await'ed not mapper task

--- c# - .net core/Realmdigital Interview/Repository/ProductVendorServiceRepository.cs
+++ c# - .net core/Realmdigital Interview/Repository/ProductVendorServiceRepository.cs
@@ -28,9 +28,9 @@ namespace Realmdigital_Interview.Repository
             {
                 client.Headers[HttpRequestHeader.ContentType] = "application/json"; //you post this type but also tell service your supported media type (xml/json/whatever) a real REST service support content-negotiation
                 client.Headers[HttpRequestHeader.Accept] = "application/json"; //content negotation (change vendor service to support compression?)
-                response = client.UploadString(_configurationData.ServiceUrl, "POST", "{ \"id\": \"" + id + "\" }"); //for this to be REST-compliant should be GET, pass in search param as querystring
+                response = await Task.Run(()=>client.UploadString(_configurationData.ServiceUrl, "POST", "{ \"id\": \"" + id + "\" }")); //await this so that we can do something else
             }
-            return  await Task.Run(()=>_mapper.Map<ApiResponseProduct, DtoApiResponseProduct>(JsonConvert.DeserializeObject<ApiResponseProduct>(response)));
+            return  _mapper.Map<ApiResponseProduct, DtoApiResponseProduct>(JsonConvert.DeserializeObject<ApiResponseProduct>(response));
         }
 
         public async Task<List<DtoApiResponseProduct>> GetProductsByName(string productName)
@@ -41,9 +41,9 @@ namespace Realmdigital_Interview.Repository
             {
                 client.Headers[HttpRequestHeader.ContentType] = "application/json";
                 client.Headers[HttpRequestHeader.Accept] = "application/json";
-                response = client.UploadString(_configurationData.ServiceUrl, "POST", "{ \"names\": \"" + productName + "\" }"); //for this to be REST-compliant should be GET, pass in search param as querystring
+                response = await Task.Run(()=>client.UploadString(_configurationData.ServiceUrl, "POST", "{ \"names\": \"" + productName + "\" }")); //should be using GET instead of POST but anyway
             }
-            return  await Task.Run(()=>_mapper.Map<List<ApiResponseProduct>, List<DtoApiResponseProduct>>(JsonConvert.DeserializeObject<List<ApiResponseProduct>>(response)));
+            return  _mapper.Map<List<ApiResponseProduct>, List<DtoApiResponseProduct>>(JsonConvert.DeserializeObject<List<ApiResponseProduct>>(response));
         }   
     }
 }
